c5e43825047e6f18af7d65ae7252776e1fd2466b
Gauravkandel <gauravkandell990@gmail.com>
2023-05-14 10:09:17 +0545

    undo last

--- proj_backend/database/migrations/2023_05_13_035831_create_p_image_table.php
+++ proj_backend/database/migrations/2023_05_13_035831_create_p_image_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("product_id");
             $table->string("image_url");
-            //this is message
             $table->timestamps();
             $table->foreign("product_id")->references('id')->on('products');
         });
