e25c08bf3f675f446e0987ed4fcea1f459b83def
Gauravkandel <gauravkandell990@gmail.com>
2023-05-16 10:25:59 +0545

    hello change

--- proj_backend/database/migrations/2023_05_10_080736_create_products_table.php
+++ proj_backend/database/migrations/2023_05_10_080736_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            //helloo
             $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("category_id");
             $table->string("pname");
